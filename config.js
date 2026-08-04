0f271681cf09fdde704403e927af20a49d0e34c1
tubankum3 <tubankum3@gmail.com>
2026-08-04 08:59:49 +0700

    Update API_URL to the new Web App URL

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // paste the Web App URL below. It looks like:
 // https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXX/exec
 const CONFIG = {
-  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwghfR7JhEJPRrlE0VXuleI6fbQi-K0HBWqPCpazVffdDpJSzn493O2N8Tu6U6ZqAZD/exec',
   // Optional — only needed if you set an API_TOKEN Script Property in
   // Code.gs (see the comment above getApiToken_ in that file). Leave as
   // '' if you didn't set one.
