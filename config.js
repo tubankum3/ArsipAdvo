4155660a23305edd4c24e849a51b379a144b55b8
tubankum3 <tubankum3@gmail.com>
2026-08-04 13:58:21 +0700

    Modify API_URL with action query parameter
    Updated API_URL to include action parameter.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // paste the Web App URL below. It looks like:
 // https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXX/exec
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwghfR7JhEJPRrlE0VXuleI6fbQi-K0HBWqPCpazVffdDpJSzn493O2N8Tu6U6ZqAZD/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwghfR7JhEJPRrlE0VXuleI6fbQi-K0HBWqPCpazVffdDpJSzn493O2N8Tu6U6ZqAZD/exec?action=perkaraSelesai',
   // Optional — only needed if you set an API_TOKEN Script Property in
   // Code.gs (see the comment above getApiToken_ in that file). Leave as
   // '' if you didn't set one.
